2b89f0808d97a2e49847b0c60e1d06ea3bfaba40
oshan <oshan@daumcorp.com>
2014-12-12 18:46:47 +0900

    Main Commit

--- src/main/java/com/g0software/g045/Main.java
+++ src/main/java/com/g0software/g045/Main.java
@@ -4,4 +4,7 @@ package com.g0software.g045;
  * Created by hanoseok on 14. 12. 12..
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("G045");
+    }
 }
